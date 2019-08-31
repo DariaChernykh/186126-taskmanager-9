1293140c45e5649ef6d00239761cb1b749814a9c
Winkii <plushka002@gmail.com>
2019-08-31 17:30:27 +0300

    add checking on new

--- src/components/absctract-component.js
+++ src/components/absctract-component.js
@@ -3,6 +3,9 @@ import {createElement} from '../utils';
 export default class AbstractComponent {
   constructor() {
     this._element = null;
+    if (new.target === AbstractComponent) {
+      throw new Error(`Can't instantiate AbstractComponent, only concrete one.`);
+    }
   }
 
   getElement() {
